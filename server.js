1fc4de496e36da9ae2d0638aef588d3b273e9b83
rebeca.prado <rebeca.2003.prado@gmail.com>
2022-05-16 09:13:33 -0300

    com conversa em tempo real

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app.set('views', path.join(__dirname, 'public'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 
+server.listen(3000)
+
 app.use('/', (req, res) => {
     res.render('index.html')
+})
+
+let messages = [];
+
+
+io.on('connection', socket => {
+    console.log(`Socket conectado ${socket.id}`)
+    socket.on('sendMessage', data=>{
+       messages.push(data);
+       socket.broadcast.emit('receivedMessage', data)
+    })
+
 })
